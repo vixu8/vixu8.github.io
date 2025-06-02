2402a2fae07e1923974b5c15d37a168f609bea23
vixu8 <viola.r.xu@gmail.com>
2025-06-02 00:21:23 -0500

    globe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { Canvas, useFrame} from '@react-three/fiber'
+import * as THREE from 'three'
 import './App.css'
 import { useRef, useState } from 'react';
 import { DirectionalLight } from 'three';
@@ -9,8 +10,16 @@ import { Stats, OrbitControls } from '@react-three/drei'
 
 function Globe() {
   const meshRef = useRef();
-  // const [isDragging, setIsDragging] = useState(false);
-  
+  const loader = new THREE.TextureLoader();
+  const globeFaces = [
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/africa2.png'), side: THREE.DoubleSide }), //right
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/pacific2.png'), side: THREE.DoubleSide }), //left
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/north2.png'), side: THREE.DoubleSide }), //top
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/antarctica2.png'), side: THREE.DoubleSide }), //bottom
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/america2.png'), side: THREE.DoubleSide }), //front
+    new THREE.MeshStandardMaterial({ map: loader.load('src/assets/asia2.png'), side: THREE.DoubleSide }), //back
+  ]
+
   useFrame((state, delta) => {
     if (meshRef.current) {
       // meshRef.current.rotation.x += delta * 0.2;
@@ -20,29 +29,42 @@ function Globe() {
 
   return <mesh
     ref={meshRef}
-    // onPointerDown={() => setIsDragging(true)}
-    // onPointerUp={() => setIsDragging(false)}
-    // onPointerMissed={() => setIsDragging(false)}
+    material={globeFaces}
     >
       <boxGeometry args={[25,25,25]} />
-      <meshStandardMaterial color="white"/>
+       
   </mesh>
 
 }
 
 function App() {
+
   const dirLight = useRef()
   const dirLight2 = useRef()
+
+  const [aspect, setAspect] = useState(window.innerWidth / (.75*window.innerHeight));
+
+  // Update aspect ratio on window resize
+  useState(() => {
+    function handleResize() {
+      setAspect(window.innerWidth / window.innerHeight);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const viewSize = 75;
 
   return (
     <div id="canvas-container">
       <Canvas 
-        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 60] }}
+        orthographic camera={{position: [0, 10, 25], left: -aspect*viewSize/2, right:aspect*viewSize/2, top:viewSize/2, bottom:-viewSize/2, zoom: 1} }
+        // camera={{ fov: 50, near: 0.1, far: 1000, position: [0, 0, 100] }}
       >
         <ambientLight intensity={.1} />
         <axesHelper />
-        <directionalLight color="blue" position={[30,0, 30]} intensity={.5} ref={dirLight}/>
-        <directionalLight color="red" position={[-30,0, 30]} intensity={.5} ref={dirLight2}/>
+        <directionalLight color="white" position={[30,20, -30]} intensity={.5} ref={dirLight}/>
+        <directionalLight color="rgb(245,237,225)" position={[-30,-10, 30]} intensity={1} ref={dirLight2}/>
 
         {dirLight.current && (
           <directionalLightHelper args={[dirLight.current, 2, 0xff0000]} />
@@ -58,8 +80,10 @@ function App() {
           enablePan={true}
           enableZoom={true}
           enableRotate={true}
-          dampingFactor={0.05}
+          dampingFactor={0.03}
           enableDamping={true}
+          // autoRotate={true}
+          // autoRotateSpeed={1.0}
         />
         <Stats />
 
