5395fa8dfd5a4e2d0f7dcddfbc65642ac50f2b1a
vixu8 <viola.r.xu@gmail.com>
2025-10-21 13:39:24 -0400

    resume update

--- src/constants/links.jsx
+++ src/constants/links.jsx
@@ -1,5 +1,5 @@
 export const resume =
-  "https://drive.google.com/file/d/1FUGQZpL_VNWhAwADi06fo0LqUogw1oRQ/view?usp=sharing";
+  "https://drive.google.com/file/d/1divgA-6FKVmDyQIR7I-i4eALKOL1fTpq/view?usp=sharing";
 export const github = "https://github.com/vixu8";
 export const email = "viola.r.xu@gmail.com";
 export const linkedin = "https://www.linkedin.com/in/vixu8/";
