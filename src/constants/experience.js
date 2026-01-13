8dc224520a27cfbdd98aa19eceef876e1cc856ff
vixu8 <viola.r.xu@gmail.com>
2026-01-13 02:05:13 -0500

    Experience section

---
+++ src/constants/experience.js
@@ -0,0 +1,26 @@
+export const experience = [
+  {
+    company: "CMU CS Academy",
+    title: "Software Development Intern",
+    date: "January 2025 - Present",
+    description:
+      "I am a software development intern at CMU CS Academy. I am working on the CS Academy website and the CS Academy app.",
+    skills: ["React", "Django", "JavaScript", "Python", "PlayWright"],
+  },
+  {
+    company: "Carnegie Mellon Racing",
+    title: "Software Developer - Special Projects Team",
+    date: "September 2025 - Present",
+    description:
+      "I am a software developer at Carnegie Mellon Racing. I am working on the Special Projects Team, which is responsible for developing the software for the car.",
+    skills: ["React", "Bootstrap"],
+  },
+  {
+    company: "HuaXia Chinese School",
+    title: "Summer Teacher",
+    date: "May 2024 - August 2024",
+    description:
+      "I am a summer teacher at HuaXia Chinese School. I am teaching math and science to students in the summer camp.",
+    skills: ["Teaching", "Leadership", "Communication"],
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,102 @@ import "../App.scss";
 import "./Home.scss";
 
 import React from "react";
-import { Link } from "react-router-dom";
 import GlobeMover from "../components/GlobeMover.jsx";
-
-import projects from "../constants/projects.jsx";
+import vxu from "../assets/VXu.png";
+import { experience } from "../constants/experience.js";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandedExperience: null,
+    };
+  }
+
+  toggleExperience = (index) => {
+    this.setState((prevState) => ({
+      expandedExperience: prevState.expandedExperience === index ? null : index,
+    }));
+  };
+
+  renderAboutSection() {
+    return (
+      <div className="about-section">
+        <h2 className="about-title">About</h2>
+        <hr />
+        <div className="about-content">
+          <div className="about-image-container">
+            <img src={vxu} alt="Viola Xu" className="about-image" />
+          </div>
+          <div className="about-text">{/* Text content goes here */}</div>
+        </div>
+      </div>
+    );
+  }
+
+  renderExperienceSection() {
+    const { expandedExperience } = this.state;
+
+    return (
+      <div className="experience-section">
+        <h2 className="about-title">Experience</h2>
+        <hr />
+        <div className="experience-list">
+          {experience.map((exp, index) => (
+            <div key={index} className="experience-item">
+              <div
+                className={`experience-header ${
+                  expandedExperience === index ? "expanded" : ""
+                }`}
+                onClick={() => this.toggleExperience(index)}
+              >
+                <svg
+                  className="experience-arrow"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 2L8 6L4 10"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <div className="experience-title-info">
+                  <div className="experience-title">{exp.title}</div>
+                  <div className="experience-company">{exp.company}</div>
+                </div>
+                <span className="experience-date">{exp.date}</span>
+              </div>
+              {expandedExperience === index && (
+                <div className="experience-content">
+                  <p className="experience-description">{exp.description}</p>
+                  <div className="tags">
+                    {exp.skills.map((skill, skillIndex) => (
+                      <span key={skillIndex} className="tag">
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    void projects;
-    void Link;
     return (
       <div>
         <GlobeMover />
-        hi
+        {this.renderAboutSection()}
+        {this.renderExperienceSection()}
       </div>
     );
   }
