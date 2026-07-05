cff0f34ba7108b5c616c6a09a290fded11d99785
vixu8 <viola.r.xu@gmail.com>
2026-07-04 20:32:35 -0500

    Update links.js

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,5 +1,5 @@
 export const resume =
-  "https://drive.google.com/file/d/1HYKF9zpyhj-G-Sk_HIJ6YlMQHVJ5ZEHe/view?usp=sharing";
+  "https://drive.google.com/file/d/1Wvl4SfvIuEh7sdOjJ3kzh-q0A3RxPW68/view?usp=sharing";
 export const github = "https://github.com/vixu8";
 export const email = "viola.r.xu@gmail.com";
 export const linkedin = "https://www.linkedin.com/in/vixu8/";
